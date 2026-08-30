78ca69e28f9e544107ffab10b74c2a5a4e21316d
Sushan <85663813+Sushan2589@users.noreply.github.com>
2026-08-30 17:58:39 +0545

    regex

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -63,7 +63,7 @@ const handleSubmit = async (
   e: React.FormEvent<HTMLFormElement>
 ) => {
   e.preventDefault();
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   if (!emailRegex.test(userInput.email)) {
   alert("Please enter a valid email address.");
